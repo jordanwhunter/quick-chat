d5ca6b11516a13ca7171e416e92d7fbd29f1fec7
jordanwhunter <jordanwhunter.mail@gmail.com>
2022-09-28 14:50:19 -0400

    (Refactor): Specify data types when creating new profile for user

--- src/server/router/user.ts
+++ src/server/router/user.ts
@@ -6,13 +6,18 @@ export const userRouter = createRouter().mutation("createUser", {
     .object({
       name: z.string(),
       contactInfo: z.string(),
+      userId: z.string(),
     })
     .nullish(),
   async resolve({ input, ctx }) {
     console.log(input);
     // TODO: persist profile data
     const profileInfo = await ctx.prisma.profile.create({
-      data: input,
+      data: {
+        name: input?.name || "",
+        contactInfo: input?.contactInfo || "",
+        userId: input?.userId
+      }
     });
     return { success: true, profile: profileInfo };
   },
