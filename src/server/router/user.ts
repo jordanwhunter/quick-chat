9a244d5af03d5d3ab745b4f5a567f0db4740a0f0
jordanwhunter <jordanwhunter.mail@gmail.com>
2022-10-07 16:28:07 -0400

    (Refactor): Add readProfile query

--- src/server/router/user.ts
+++ src/server/router/user.ts
@@ -1,24 +1,37 @@
 import { createRouter } from "./context";
 import { z } from "zod";
 
-export const userRouter = createRouter().mutation("createUser", {
-  input: z
-    .object({
+export const userRouter = createRouter()
+  .mutation("createUser", {
+    input: z.object({
       name: z.string(),
       contactInfo: z.string(),
       userId: z.string(),
-    })
-    .nullish(),
-  async resolve({ input, ctx }) {
-    console.log(input);
-    // TODO: persist profile data
-    const profileInfo = await ctx.prisma.profile.create({
-      data: {
-        name: input?.name || "",
-        contactInfo: input?.contactInfo || "",
-        userId: input?.userId
-      }
-    });
-    return { success: true, profile: profileInfo };
-  },
-});
+    }),
+    // .nullish(),
+    async resolve({ input, ctx }) {
+      console.log(input);
+
+      const profileInfo = await ctx.prisma.profile.create({
+        data: {
+          name: input?.name,
+          contactInfo: input?.contactInfo,
+          userId: input?.userId,
+        },
+      });
+      return { success: true, profile: profileInfo };
+    },
+  })
+  .query("readProfile", {
+    input: z.object({
+      userId: z.string(),
+    }),
+    async resolve({ input, ctx }) {
+      const profile = await ctx.prisma.profile.findUnique({
+        where: {
+          userId: input?.userId
+        }
+      })
+      return { success: true, profile: profile}
+    },
+  });
