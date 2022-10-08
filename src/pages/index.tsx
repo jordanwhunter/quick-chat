bcbf3365c1e53fe6cf8a4ea57f3f76525d5b7d69
jordanwhunter <jordanwhunter.mail@gmail.com>
2022-10-07 22:12:31 -0400

    (Docs): Add TODO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,15 @@ import { signIn, useSession } from "next-auth/react";
 import { Profile } from "../components/Profile";
 import { Puff } from "react-loading-icons";
 
+
 const Home: NextPage = () => {
   const { data: session, status } = useSession();
 
   if (status === "unauthenticated") {
     signIn();
   }
+
+  // TODO: Push away from this page if user has created profile and tries to return here (grab verification from useProfile hook)
 
   return (
     <>
@@ -22,6 +25,15 @@ const Home: NextPage = () => {
       <main>
         {session ? (
           <Profile />
+          // <div>
+          //   {profile.name === undefined ? (
+          //     <Profile />
+          //   ) : (
+          //     <div className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
+          //       <h2 className="text-white">{profile.name}</h2>
+          //     </div>
+          //   )}
+          // </div>
         ) : (
           <div className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
             <Puff
